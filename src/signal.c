d1c6daff8c4be7bb100e588defa76da7cfc17578
Seungjun Kim <goodkim97@gmail.com>
2017-11-05 18:54:31 +0900

    before pull from forked master

--- src/signal.c
+++ src/signal.c
@@ -1,5 +1,6 @@
 #include "signal.h"
 #include <signal.h>
+#include <stdio.h>
 
 void catch_sigint(int num)
 {
@@ -12,3 +13,8 @@ void catch_sigtstp(int num)
   // TODO: File this!
   signal(SIGTSTP,SIG_IGN);
 }
+
+void test_func(){
+  printf("test function runed!");
+}
+
